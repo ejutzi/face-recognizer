28421efa132e55b82c23cd84203d68f7ab9ffce7
Naikx <erik.jutzi@googlemail.com>
2020-06-07 16:23:05 +0200

    Updates

---
+++ precache-manifest.9212141bcc6df4b300d97b84aaa60212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c07abd953c77f391d4a2541497bcc99",
+    "url": "/face-recognizer/index.html"
+  },
+  {
+    "revision": "e72881ce33c8440f609e",
+    "url": "/face-recognizer/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "97096f810bfd87a7a3f1",
+    "url": "/face-recognizer/static/css/main.7336200f.chunk.css"
+  },
+  {
+    "revision": "e72881ce33c8440f609e",
+    "url": "/face-recognizer/static/js/2.45750679.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognizer/static/js/2.45750679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97096f810bfd87a7a3f1",
+    "url": "/face-recognizer/static/js/main.91c684c6.chunk.js"
+  },
+  {
+    "revision": "423c277e454e75e32f4d",
+    "url": "/face-recognizer/static/js/runtime-main.f597a8b3.js"
+  }
+]);
